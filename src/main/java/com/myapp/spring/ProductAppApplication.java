a0abf6a31d88598935d2ad778935b8b48441c163
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2021-08-27 15:49:38 +0530

    first commit

---
+++ src/main/java/com/myapp/spring/ProductAppApplication.java
@@ -0,0 +1,21 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication()
+
+public class ProductAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAppApplication.class, args);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
